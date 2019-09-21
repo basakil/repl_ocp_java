aff54c2dd384239c462755ff380718cd0e6f962c
basakil <basakil@gmail.com>
2019-09-21 18:50:51 +0300

    initial commit.

---
+++ src/main/java/tr/nom/aob/ocpjava/Identifiers.java
@@ -0,0 +1,7 @@
+package tr.nom.aob.ocpjava;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Hello Identifiers..");
+    }
+}
---
+++ src/test/java/tr/nom/aob/ocpjava/test/IdentifierTests.java
@@ -0,0 +1,16 @@
+package tr.nom.aob.ocpjava.test;
+
+public class IdentifierTests {
+
+    public void test1() {
+        int iden_$tfier4 = 4;
+        int iden€tif$ier = 5;
+//        byte 4idenfier = 5;
+        byte $idenfier = 5;
+        byte €idenfier = 5;
+        byte £idenfier = 5;
+        byte _idenfier = 5;
+        byte _idenFier = 5;
+
+    }
+}
